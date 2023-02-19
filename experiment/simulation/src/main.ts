1e958f4abb88280737e7f418a24c688cbb50388a
Aarush Jain <aarushj09@gmail.com>
2023-02-19 14:35:54 +0530

    advance clock and process circle display done

--- experiment/simulation/src/main.ts
+++ experiment/simulation/src/main.ts
@@ -1,13 +1,16 @@
 import { UI } from "./UI";
 const ui = new UI();
-ui.kernel.createProcess();
-ui.display_processes();
-ui.kernel.createProcess();
-ui.display_processes();
-ui.kernel.createProcess();
-ui.display_processes();
-// console.log(kernel.processes)
-// console.log(kernel.get_terminatable_procs());
-// // kernel.terminate(1);
-// console.log(kernel.get_terminatable_procs());
-// console.log(kernel.external_events);
+ui.createProcess();
+ui.createProcess();
+ui.createProcess();
+ui.createProcess();
+ui.createProcess();
+ui.createProcess();
+ui.createProcess();
+ui.createProcess();
+
+
+
+document.getElementById("clock").addEventListener("click", () => {
+    ui.update_clock();
+});
