ff6d0ddea093581f7898a21e1d378d65f91f9882
Jyotheeswar114 <jyotheeswarganne60@gmail.com>
2023-04-10 00:17:39 +0530

    analytics pdf name will be in format ddmmyyyy_hhmmss

--- experiment/simulation/src/UI.ts
+++ experiment/simulation/src/UI.ts
@@ -250,6 +250,7 @@ class UI {
         }
     }
 
+
     display_analytics() {
         let analytics = document.getElementById("analytics");
         analytics.innerHTML = "";
@@ -266,6 +267,7 @@ class UI {
         analytics.appendChild(cpuIdle);
         analytics.appendChild(cpuIOIdle);
     }
+
 
     display_log() {
         let log = document.getElementById("log");
@@ -445,7 +447,16 @@ class UI {
             this.display_all();
         };
 
-
+        let formatDate = () => {
+            const date = new Date();
+            const day = String(date.getDate()).padStart(2, '0');
+            const month = String(date.getMonth() + 1).padStart(2, '0');
+            const year = String(date.getFullYear());
+            const hours = String(date.getHours()).padStart(2, '0');
+            const minutes = String(date.getMinutes()).padStart(2, '0');
+            const seconds = String(date.getSeconds()).padStart(2, '0');
+            return `${day}${month}${year}_${hours}${minutes}${seconds}`;
+          }
         let finish_button_handler = () => {
             const { jsPDF } = require("jspdf"); // will automatically load the node version
             const confirmed: boolean = window.confirm("Do you want to download your analytics ?");
@@ -463,7 +474,7 @@ class UI {
                 doc.text(`4. CPU IO Wait time: ${this.kernel.getCPUIOWaitTime()}`, 20, 80);
                 doc.text("Observation Table", 20, 90)
                 autoTable(doc, { html: '#log', startY: 100 });
-                doc.save("a4.pdf");
+                doc.save(`${formatDate()}.pdf`);
             } else {
             // User clicked Cancel
             }
