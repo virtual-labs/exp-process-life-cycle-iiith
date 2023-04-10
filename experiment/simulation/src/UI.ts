b1b3068a055fe65448d622efc72eae13c09830fa
Ayan Agrawal <Ayan@Ayans-MacBook-Air.local>
2023-04-10 07:33:35 +0530

    Popup window

--- experiment/simulation/src/UI.ts
+++ experiment/simulation/src/UI.ts
@@ -8,6 +8,9 @@ const html2canvas = require("html2canvas");
 // require('jspdf-autotable');
 import autoTable from "jspdf-autotable";
 import Chart from "chart.js";
+// import material/dialog
+import { MDCDialog } from "@material/dialog";
+import { MDCRipple } from "@material/ripple";
 
 export { UI };
 class UI {
@@ -241,6 +244,95 @@ class UI {
     }
   }
 
+  display_theory() {
+    let theory = document.getElementById("theory");
+    theory.innerHTML = "";
+
+    // dialog box for theory using @material/dialog with button
+    let dialog = document.createElement("dialog");
+    dialog.classList.add("mdl-dialog");
+    let title = document.createElement("h4");
+    title.classList.add("mdl-dialog__title");
+    title.innerText = "What is a process?";
+    let content = document.createElement("div");
+    content.classList.add("mdl-dialog__content");
+    content.innerHTML = "<p>In computing, a process is an instance of a computer program that is being executed by a computer's operating system (OS). A process consists of the program's code and its current activity, such as the values of its variables, the state of its I/O channels, and its position in memory.</p><br /><p>Processes are managed by the operating system, which allocates resources such as CPU time, memory, and I/O devices to each process as needed. The OS also provides mechanisms for inter-process communication, synchronization, and coordination.</p><br /><p>Each process is assigned a unique identifier, known as a process ID (PID), which can be used to identify and manage the process. The operating system maintains a process table that stores information about each running process, such as its PID, its priority, and its resource usage.</p><br /><p>In a multitasking operating system, multiple processes may be running simultaneously, sharing the system resources. The OS schedules the execution of these processes, switching between them rapidly to give the illusion of parallel execution. This allows multiple programs to be run concurrently, which can improve the overall performance of the system.</p><br />";
+    let actions = document.createElement("div");
+    actions.classList.add("mdl-dialog__actions");
+    let close = document.createElement("button");
+    close.classList.add("mdl-button");
+    close.classList.add("close");
+    close.innerText = "Close";
+    close.addEventListener("click", () => {
+        dialog.close();
+        }
+    );
+    actions.appendChild(close);
+    dialog.appendChild(title);
+    dialog.appendChild(content);
+    dialog.appendChild(actions);
+    theory.appendChild(dialog);
+
+    // button to open dialog box
+    let button = document.createElement("button");
+    button.classList.add("mdl-button");
+    button.classList.add("mdl-js-button");
+    button.classList.add("mdl-button--raised");
+    button.classList.add("mdl-button--colored");
+    button.innerText = title.innerText;
+    button.addEventListener("click", () => {
+        dialog.showModal();
+    }
+    );
+    theory.appendChild(button);
+  }
+
+  display_theory2() {
+    let theory = document.getElementById("theory2");
+    theory.innerHTML = "";
+
+    // dialog box for theory using @material/dialog with button
+    let dialog = document.createElement("dialog");
+    dialog.classList.add("mdl-dialog");
+    let title = document.createElement("h4");
+    title.classList.add("mdl-dialog__title");
+    title.innerText = "What are process states?";
+    // add a blank line
+    let br = document.createElement("br");
+    let content = document.createElement("div");
+    content.classList.add("mdl-dialog__content");
+    content.innerHTML = "<p>As a process is executed, it undergoes a series of state changes that reflect the activity being performed by the user and the resources needed by the process. The specific states and their corresponding names can vary between different operating systems and literature sources, as they are not standardized. Nonetheless, the process state provides crucial information about the current status of a process and is used by the operating system to manage resources and scheduling.</p><p>The 4 main and most common states the process can exist as are:</p><ul><li>Ready: A process in the ready state is one that is waiting to be executed by the CPU, but is currently not running. The process is waiting for the CPU to allocate resources to it, and is typically waiting in a queue for its turn to run.</li><li>Running: When a process is executing instructions on the CPU, it is in the running state. At any given time, there may be only one process in the running state on a single CPU.</li><li>Waiting : If the process is in this state then it is waiting for either resources that it has requested for or waiting for a specific event to occur so that it can go back to ready state and wait for dispatching The process is not using the CPU during this time and may be waiting for an indefinite period.</li><li>Terminated: When a process has completed its execution or has been terminated by the operating system or by the user, it is in the terminated state. The process may still have some resources allocated to it, but it is no longer running.</li></ul>";
+    let actions = document.createElement("div");
+    actions.classList.add("mdl-dialog__actions");
+    let close = document.createElement("button");
+    close.classList.add("mdl-button");
+    close.classList.add("close");
+    close.innerText = "Close";
+    close.addEventListener("click", () => {
+        dialog.close();
+        }
+    );
+    actions.appendChild(close);
+    dialog.appendChild(title);
+    dialog.appendChild(br);
+    dialog.appendChild(content);
+    dialog.appendChild(actions);
+    theory.appendChild(dialog);
+
+    // button to open dialog box
+    let button = document.createElement("button");
+    button.classList.add("mdl-button");
+    button.classList.add("mdl-js-button");
+    button.classList.add("mdl-button--raised");
+    button.classList.add("mdl-button--colored");
+    button.innerText = title.innerText;
+    button.addEventListener("click", () => {
+        dialog.showModal();
+    }
+    );
+    theory.appendChild(button);
+  }
+
   display_analytics() {
     let analytics = document.getElementById("analytics");
     analytics.innerHTML = "";
@@ -362,6 +454,8 @@ class UI {
     this.display_log();
     this.update_accordion();
     this.display_analytics();
+    this.display_theory();
+    this.display_theory2();
     this.console_display();
     if (
       this.kernel.selectedEvent !== -1 &&
