4c143b3fdc189afcc71ef403ba2e5dac4c6f9bf4
souvik ray <souvikray97@gmail.com>
2025-01-16 20:45:20 +0530

    built

---
+++ build/exp-process-life-cycle-iiith/simulation/src/Process.ts
@@ -0,0 +1,69 @@
+import * as config from "./config"
+
+interface IOTime {
+    start_time: number
+    ticks: number
+}
+
+export interface IProcess {
+    pid: number;
+    name: string;
+    state: string;
+    history: string[]
+}
+
+export class Process implements IProcess {
+    pid: number;
+    name: string;
+    state: string;
+    history: string[]
+
+    constructor(pid: number = 0) {
+        this.pid = pid;
+        this.name = "P" + String(pid);
+        this.state = config.INFANT;
+        this.history = [];
+    }
+
+    getData(): IProcess {
+        return {
+            "pid": this.pid,
+            "name": this.name,
+            "state": this.state,
+            "history": this.history
+        }
+    }
+
+    setData(data: IProcess) {
+        const {pid, name, state, history} = data;
+        this.pid = pid;
+        this.name = name;
+        this.state = state;
+        this.history = history;
+    }
+
+    create() {
+        this.state = config.READY;
+    }
+
+    run() {
+        this.state = config.RUNNING;
+        this.history.push(this.state);
+    }
+
+    ready() {
+        this.state = config.READY;
+        this.history.push(this.state);
+    }
+
+    moveToIO() {
+        this.state = config.BLOCKED;
+        this.history.push(this.state);
+    }
+
+    terminate() {
+        this.state = config.TERMINATED;
+        this.history.push(this.state);
+    }
+    
+}
